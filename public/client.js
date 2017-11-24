ee8932747fbde9f27c1f6aa1b45f4b7cc9a719f3
Glitch (deadpan-seal) <none>
2017-11-24 19:15:31 +0000

    🕌☕️ Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -4,12 +4,23 @@
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
 
+var formatResp = function(data, cont){
+  cont.empty();
+  for (var key in data) {
+    var row = $('<div class = resp_row></div>');
+    $('<span class=resp_key></span>').text(key + ':').appendTo(row);
+    $('<span class=resp_val></span>').text(data[key]).appendTo(row);
+    cont.append(row);
+  };
+}
+
 $(function() {
 
   $('form').submit(function(event) {
     event.preventDefault();
     var href = $('input').val();
-    $.post('?' + $.param({href: href}), function() {
+    $.post('?' + $.param({href: href}), function(data) {
+      formatResp(data, $('#response_cont'));
       $('input').val('');
       $('input').focus();
     });
